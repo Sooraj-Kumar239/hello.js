c66a4af7a3f7fe30d9ced0ce7c4c563b2376d92f
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-01-30 17:50:43 +0500

    test.js

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 alert("Hello World!");
+
+function ab(f,m){
+    let sum=0;
+
+    for(let i=0; i < m.length; i++) {
+            for (let j=0; j < f.length; j++){
+                
+                if(m[i]  %  f[j] ===0)
+                {
+                    sum += m[i];
+                    break;
+                }
+            }
+    }
+    return sum;
+}
+
+console.log(ab([3,5],[1,2,3,4,5,6,7,8,9]));
