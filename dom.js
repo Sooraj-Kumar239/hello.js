e2f2b120903dd37ce0813af72980e6cbcd40c3e7
Sooraj <skoad.2194@gmail.com>
2026-02-07 09:04:26 -1200

    dom practice working

---
+++ dom.js
@@ -0,0 +1,22 @@
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("data").innerHTML = `
+      <p><b>Name:</b> ${data.name}</p>
+      <p><b>Email:</b> ${data.email}</p>
+      <p><b>City:</b> ${data.address.city}</p>
+    `;
+  })
+  .catch(error => {
+    document.getElementById("data").innerHTML = "Error loading data";
+    console.error(error);
+  });
+
+    
+    function show(event){
+    event.preventDefault();
+    
+        let text = document.getElementById("input").value; 
+
+        document.getElementById("output").innerHTML=text;
+    }
